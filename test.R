7dc6ca4eeda2baa24365c7490899b677dfd1eb2b
Macarena Estevez <36417794+mestevez1966@users.noreply.github.com>
2022-06-20 15:13:51 +0200

    Update test.R

--- test.R
+++ test.R
@@ -9,7 +9,7 @@ library(RSQLite)
 auth <- rtweet::rtweet_app(bearer_token = Sys.getenv("BEARER_TOKEN"))
 
 token <- Sys.getenv("MEANING_TOKEN")
-meaningcloud <- function(texto, token = token)) {
+meaningcloud <- function(texto, token = token) {
   # Post
   h <- curl::new_handle()
   curl::handle_setform(h,
